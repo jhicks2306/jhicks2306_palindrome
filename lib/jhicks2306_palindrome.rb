3094659d05d927f9e4cebe7e78622db96d1f2a91
James Hicks <jhicks2306@gmail.com>
2021-11-07 20:56:40 +0000

    Add tests to test file

--- lib/jhicks2306_palindrome.rb
+++ lib/jhicks2306_palindrome.rb
@@ -2,7 +2,22 @@
 
 require_relative "jhicks2306_palindrome/version"
 
-module Jhicks2306Palindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# module Jhicks2306Palindrome
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content.downcase == processed_content.reverse
+  end
+
+  private
+
+  # Returns content for palindrome testing
+  def processed_content
+    self.to_s.downcase
+  end
+  
 end
--- test/jhicks2306_palindrome_test.rb
+++ test/jhicks2306_palindrome_test.rb
@@ -7,7 +7,19 @@ class Jhicks2306PalindromeTest < Minitest::Test
     refute_nil ::Jhicks2306Palindrome::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_non_palindrome
+    assert !"apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    skip
+  end
+
+  def test_palindrome_with_punctuation
+    skip
   end
 end
