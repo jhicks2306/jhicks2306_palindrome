097907fe7092b63ff7c4089731398fbdbab7ae90
James Hicks <jhicks2306@gmail.com>
2021-11-07 20:13:39 +0000

    Initialize repository

---
+++ lib/jhicks2306_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jhicks2306_palindrome/version"
+
+module Jhicks2306Palindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/jhicks2306_palindrome_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Jhicks2306PalindromeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Jhicks2306Palindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
